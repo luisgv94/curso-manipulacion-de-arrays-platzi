47292e63d3c98790f3b8d855e10b4a33f8a52669
luisgv94 <lgomero94@gmail.com>
2023-03-03 22:58:47 -0500

    reto every

--- server/09-every.js
+++ server/09-every.js
@@ -1,33 +1,34 @@
-const numbers = [1,30,39,29,10,13];
-
+const numbers = [1, 30, 39, 29, 10, 13];
 
 let rta = true;
 for (let index = 0; index < numbers.length; index++) {
-    const element = numbers[index];
-    if (element >= 40) {
-        rta = false
-    }
+  const element = numbers[index];
+  if (element >= 40) {
+    rta = false;
+  }
 }
 console.log('for', rta);
-const rta2 = numbers.every(item => item <= 40)
+const rta2 = numbers.every((item) => item <= 40);
 console.log('rta2', rta2);
-
 
 const team = [
   {
-    name: "Nicolas",
+    name: 'Nicolas',
     age: 12,
   },
   {
-    name: "Andrea",
+    name: 'Andrea',
     age: 8,
   },
   {
-    name: "Zulema",
+    name: 'Zulema',
     age: 2,
   },
   {
-    name: "Santiago",
+    name: 'Santiago',
     age: 18,
   },
 ];
+
+const rta3 = team.every((member) => member.age < 15);
+console.log('rta3', rta3);
