bca696ef668985f4f35a34a235bea9e473d1670b
luisgv94 <lgomero94@gmail.com>
2023-03-04 00:24:15 -0500

    update flat

--- server/14-flat.js
+++ server/14-flat.js
@@ -1,17 +1,28 @@
 const matriz = [
-    [1,2,3],
-    [4,5,6, [1,2, [1,2]]],
-    [7,8,9]
+  [1, 2, 3],
+  [4, 5, 6, [1, 2, [1, 2]]],
+  [7, 8, 9],
 ];
 
 const newArray = [];
 for (let i = 0; i < matriz.length; i++) {
-    const array = matriz[i];
-    for (let j = 0; j < array.length; j++) {
-        const element = matriz[i][j];
-        newArray.push(element);
-    }
+  const array = matriz[i];
+  for (let j = 0; j < array.length; j++) {
+    const element = matriz[i][j];
+    newArray.push(element);
+  }
 }
 const rta = matriz.flat(3);
 console.log('for', newArray);
 console.log('flat', rta);
+
+function profundidad(list) {
+  let newList = [];
+  if (typeof list != 'object') return [list];
+  list.forEach((element) => {
+    newList = newList.concat(profundidad(element));
+  });
+  return newList;
+}
+
+console.log('recursion for', profundidad(matriz));
